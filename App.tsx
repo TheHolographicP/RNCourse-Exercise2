3b4e49cbd2149cd9ee43511a1469f5f3045462fb
TheHolographicP <thegamblerkid@gmail.com>
2026-03-12 14:01:41 -0600

    Add gradient and image background;

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import StartGameScreen from 'screens/StartGameScreen'
-
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <StartGameScreen/>
+    <LinearGradient style={styles.rootScreen} colors={['#EF8354','#4F5D75']}>
+      <ImageBackground
+        source={require('assets/background.png')}
+        resizeMode='cover'
+        style={styles.imageBkg}
+        imageStyle={{opacity:0.15}}
+      >
+        <StartGameScreen/>
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  rootScreen:{
+    flex:1,
+  },
+  imageBkg: {
+    flex:1,
+    paddingTop:50,
+    paddingHorizontal:16,
+  }
 });
