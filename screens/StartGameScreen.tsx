79d943b370da0655ccf50ebd59170c443fba8f50
TheHolographicP <thegamblerkid@gmail.com>
2026-03-12 11:18:39 -0600

    Add validation to number input field;

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,33 +1,46 @@
+import { useState } from 'react';
 import { TextInput, Text, View, StyleSheet} from 'react-native';
 import PrimaryButton from 'components/PrimaryButton'
 
 
 function StartGameScreen() {
+    const [numberInputText, setNumberInputText] = useState('')
+
+    function inputHandler(enteredText: string): void {
+        if (/^[0-9]*$/.test(enteredText))
+            setNumberInputText(enteredText)
+    }
 
     function handleReset(){
+        setNumberInputText('')
+    }
+
+    function handleSubmit(){
 
     }
 
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.targetInput}/>
-            <View style={styles.buttonGroup}>
-                <PrimaryButton callOnPress={handleReset} displayText='Reset' style={styles.button}/>
-                <PrimaryButton callOnPress={handleReset} displayText='Confirm' style={styles.button}/>
-            </View>
+            <TextInput
+                style={styles.targetInput}
+                value={numberInputText}
+                onSubmitEditing={handleSubmit}
+                onChangeText={inputHandler}
+                maxLength={2}
+                keyboardType='number-pad'
+                
+            />
+        
+            <PrimaryButton callOnPress={handleReset} displayText='Reset' style={styles.button}/>
+            <PrimaryButton callOnPress={handleReset} displayText='Confirm' style={styles.button}/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'column',
-        gap:10,
         paddingTop:50,
         paddingHorizontal:16,
-        alignItems: 'stretch'
-    },
-    buttonGroup:{
         flexDirection: 'row',
         alignItems: 'stretch',
         gap:10
@@ -36,6 +49,7 @@ const styles = StyleSheet.create({
         flex:1
     },
     targetInput:{
+        flex:1,
         borderWidth: 1,
         borderRadius: 6,
         padding:7,
